759074c9c07891274b4ff69e4d77dac5a50fcd80
Ahmed Kennawi <ahmed.kennawi@yahoo.com>
2023-04-18 20:20:25 +0200

    updated:task-interview

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 
 export default function App() {
   const [data, setData] = useState();
-  console.log(data, "data");
 
   const getData = (url) => {
     fetch(url)
@@ -21,7 +20,9 @@ export default function App() {
         <h1 className="text-7xl font-normal mb-10">Responsive dynamic cards</h1>
         <div className="lg:flex-row flex items-center justify-between gap-8 flex-col ">
           {data?.map((card, index) => (
+            // card
             <div key={index} className=" w-[345.33px] rounded-xl shadow-2xl ">
+              {/*  image  */}
               <div>
                 <img
                   className="rounded-t-xl "
@@ -29,17 +30,24 @@ export default function App() {
                   alt="card img"
                 />
               </div>
+              {/* end image */}
+              {/* body */}
               <div className="flex flex-col justify-between h-[344px] p-4">
                 <div className="p-4">
                   <h2 className="font-bold text-xl ">{card.title}</h2>
-                  <h3 className="city_card">{card.city}</h3>
+                  <div className="flex items-center">
+                    <img className="w-5 h-5" src="/location.svg" />
+                    <h3 className="font-normal text-xl ">{card.city}</h3>
+                  </div>
                   <p className="title_description">{card.desc}</p>
                 </div>
                 <button className="bg-[#FFE000] w-full rounded-lg py-[10px]">
                   Read more
                 </button>
               </div>
+              {/* end body */}
             </div>
+            // end card
           ))}
         </div>
       </div>
